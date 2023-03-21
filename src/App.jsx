9701a97f7203675473cfe065c13025c54dea1787
David <dndinh7@gmail.com>
2023-03-21 00:59:01 -0400

    got the backend working to get the api of users and got react to fetch the users from there and display it on the page. had to use enable CORS to allow the react to get request the post api... also created some components for the homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-
-import './App.css'
+import Home from './components/Home';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      
+			<Home />
     </div>
   )
 }
 
-export default App
+export default App;
