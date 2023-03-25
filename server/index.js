0e840a47a828a6ce2c70f7b9017649502c6a6559
Tung Do <tdo@haverford.edu>
2023-03-25 19:57:28 -0400

    create post form

--- server/index.js
+++ server/index.js
@@ -1,6 +1,11 @@
 let express= require('express');
 let app= express();
 let cors= require('cors');
+var mongoose = require('mongoose');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.use(cors());
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Home from './components/Home';
 import './App.css';
+import CreatePostForm from './components/CreatePostForm';
 
 function App() {
 
   return (
     <div className="App">
 			<Home />
+      <CreatePostForm />
     </div>
   )
 }
