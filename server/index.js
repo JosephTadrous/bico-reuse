9703659a0614cb5e397de9f9004466f54ed9f97d
Mai Anh Nguyen <67620927+maianhtrnguyen@users.noreply.github.com>
2023-03-25 15:42:23 -0400

    finished viewing story

--- server/index.js
+++ server/index.js
@@ -29,6 +29,43 @@ app.get("/api", (req, res) => {
 	});
 });
 
+// endpoint for viewing a list of items for sale from other sellers on the homepage
+app.use("/all", (req, res) => {
+	// find all the Post objects in the database
+	Post.find()({}, (err, posts) => {
+		if (err) {
+		    res.type('html').status(200);
+		    console.log(err);
+		    res.write(err);
+		} else {
+			if (posts.length == 0) {
+				res.type('html').status(200);
+				res.write('There are no items for sale!');
+				res.end();
+				return;
+			} else {
+				res.type('html').status(200);
+				res.write('Here are the list of items for sale on the homepage:');
+				res.write('<ul>');
+				// show all the items
+				posts.forEach( (post) => {
+					res.write('<li>');
+					res.write('Name: ' + post.seller); 
+					res.write('Title: ' + post.title);
+					res.write('Date: ' + post.date); 
+					res.write('Description: ' + post.description);
+					res.write('Price: ' + post.price);
+					res.write('Photos: ' + post.photos); 
+					res.write('Status: ' + post.status);
+					res.write('</li>');
+				});
+				res.write('</ul>');
+				res.end();
+		    }
+		}
+	}).sort({ 'Date': 'asc' }); // sorting by date before rendering the results
+});
+
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
 });
