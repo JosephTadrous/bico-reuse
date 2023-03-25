03be69ea07fa220b1501a52f4995ee31a920a07a
David <dndinh7@gmail.com>
2023-03-25 02:15:18 -0400

    styled the posts on the homepage

--- src/components/HomePost.jsx
+++ src/components/HomePost.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 
 
 export default function HomePost({post}) {
 	return (
 		<div className="HomePost">
-			<h1>{post.title} - ${post.price}</h1>
-			<p>{post.seller.name}</p>
+			<NavLink className="Container" to= {'/post?pid=' + post._id} >
+				<h1>{post.title} - ${post.price}</h1>	
+				<p>{post.seller.name}</p>
+			</NavLink> 
 		</div>
 	)
 }
--- src/components/HomePostList.jsx
+++ src/components/HomePostList.jsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import HomePost from './HomePost';
 import './Home.css'; 
-import { NavLink } from 'react-router-dom';
 
 export default function HomePostList({posts}) {
 	return ( <ul className="PostLinks">{
 		posts.map((post) => {
 			return <li key={post._id}> 
-				<NavLink to= {'/post?pid=' + post._id} >
-					<HomePost post={post}/>
-				</NavLink> 
+				<HomePost post={post}/>
 			</li>
 		})
 		}</ul>
