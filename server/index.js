6d6120b29533bc9f678efbb9e49505d8126b3885
Mai Anh Nguyen <67620927+maianhtrnguyen@users.noreply.github.com>
2023-03-25 19:12:14 -0400

    modified user story 1

--- server/index.js
+++ server/index.js
@@ -76,44 +76,6 @@ app.use("/all", (req, res) => {
 	});
 });
 
-
-
-	// Post.find()({}, (err, posts) => {
-	// 	if (err) {
-	// 	    res.type('html').status(200);
-	// 	    console.log(err);
-	// 	    res.write(err);
-	// 	} else {
-	// 		if (posts.length == 0) {
-	// 			res.type('html').status(200);
-	// 			res.write('There are no items for sale!');
-	// 			res.end();
-	// 			return;
-	// 		} else {
-	// 			res.type('html').status(200);
-	// 			res.write('Here are the list of items for sale on the homepage:');
-	// 			res.write('<ul>');
-	// 			// show all the items
-	// 			posts.forEach( (post) => {
-	// 				res.write('<li>');
-	// 				res.write('Name: ' + post.seller); 
-	// 				res.write('Title: ' + post.title);
-	// 				res.write('Date: ' + post.date); 
-	// 				res.write('Description: ' + post.description);
-	// 				res.write('Price: ' + post.price);
-	// 				res.write('Photos: ' + post.photos); 
-	// 				res.write('Status: ' + post.status);
-	// 				res.write('</li>');
-	// 			});
-	// 			res.write('</ul>');
-	// 			res.end();
-	// 	    }
-	// 	}
-	// })
-// });
-
-// app.use('/', (req, res) => { res.redirect('../index.html'); } );
-
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
 });
