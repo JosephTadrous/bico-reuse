06d3b8fe3e916a0ebdea1b4848abf1c0d3e68034
Mai Anh Nguyen <67620927+maianhtrnguyen@users.noreply.github.com>
2023-03-25 19:12:04 -0400

    modified use

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ let express= require('express');
 let app= express();
 let cors= require('cors');
 
+// set up BodyParser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(cors());
 
 let User= require('./dbfiles/User.js');
@@ -31,40 +35,84 @@ app.get("/api", (req, res) => {
 
 // endpoint for viewing a list of items for sale from other sellers on the homepage
 app.use("/all", (req, res) => {
-	// find all the Post objects in the database
-	Post.find()({}, (err, posts) => {
-		if (err) {
-		    res.type('html').status(200);
-		    console.log(err);
-		    res.write(err);
-		} else {
-			if (posts.length == 0) {
-				res.type('html').status(200);
-				res.write('There are no items for sale!');
-				res.end();
-				return;
-			} else {
-				res.type('html').status(200);
-				res.write('Here are the list of items for sale on the homepage:');
-				res.write('<ul>');
-				// show all the items
-				posts.forEach( (post) => {
-					res.write('<li>');
-					res.write('Name: ' + post.seller); 
-					res.write('Title: ' + post.title);
-					res.write('Date: ' + post.date); 
-					res.write('Description: ' + post.description);
-					res.write('Price: ' + post.price);
-					res.write('Photos: ' + post.photos); 
-					res.write('Status: ' + post.status);
-					res.write('</li>');
-				});
-				res.write('</ul>');
-				res.end();
-		    }
-		}
-	}).sort({ 'Date': 'asc' }); // sorting by date before rendering the results
+	// get all documents
+	let posts= Post.find()
+	.populate('seller')
+	.then(
+		// success
+		(post) => {
+			if (post) {
+				if (Array.isArray(post)) {
+					// res.json(post);
+					// res.write('<ul>');
+					post.forEach( (p) => {
+						// res.write('<li>');
+						res.write('Name: ' + p.seller + '\n'); 
+						res.write('Title: ' + p.title + '\n');
+						res.write('Date: ' + p.date + '\n'); 
+						res.write('Description: ' + p.description + '\n');
+						res.write('Price: ' + p.price + '\n');
+						res.write('Photos: ' + p.photos + '\n'); 
+						res.write('Status: ' + p.status + '\n\n\n');
+						res.write('</li>');
+					});
+					// res.write('<ul>');
+				} else { // only one post exists
+					var p = post[0];
+					res.write('Name: ' + p.seller + '\n'); 
+					res.write('Title: ' + p.title + '\n');
+					res.write('Date: ' + p.date + '\n'); 
+					res.write('Description: ' + p.description + '\n');
+					res.write('Price: ' + p.price + '\n');
+					res.write('Photos: ' + p.photos + '\n'); 
+					res.write('Status: ' + p.status + '\n\n\n');
+				}
+			} else { // undefined
+				res.json([]);
+			}
+		}, // failure
+		(error) => {
+			res.status(500).send('Something went wrong');
+	});
 });
+
+
+
+	// Post.find()({}, (err, posts) => {
+	// 	if (err) {
+	// 	    res.type('html').status(200);
+	// 	    console.log(err);
+	// 	    res.write(err);
+	// 	} else {
+	// 		if (posts.length == 0) {
+	// 			res.type('html').status(200);
+	// 			res.write('There are no items for sale!');
+	// 			res.end();
+	// 			return;
+	// 		} else {
+	// 			res.type('html').status(200);
+	// 			res.write('Here are the list of items for sale on the homepage:');
+	// 			res.write('<ul>');
+	// 			// show all the items
+	// 			posts.forEach( (post) => {
+	// 				res.write('<li>');
+	// 				res.write('Name: ' + post.seller); 
+	// 				res.write('Title: ' + post.title);
+	// 				res.write('Date: ' + post.date); 
+	// 				res.write('Description: ' + post.description);
+	// 				res.write('Price: ' + post.price);
+	// 				res.write('Photos: ' + post.photos); 
+	// 				res.write('Status: ' + post.status);
+	// 				res.write('</li>');
+	// 			});
+	// 			res.write('</ul>');
+	// 			res.end();
+	// 	    }
+	// 	}
+	// })
+// });
+
+// app.use('/', (req, res) => { res.redirect('../index.html'); } );
 
 app.listen(3000, () => {
 	console.log('Listening on port 3000');
