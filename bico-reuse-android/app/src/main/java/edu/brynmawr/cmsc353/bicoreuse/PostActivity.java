53b0f31c85cd3a8f89a5d90ca03415c240f5482a
David Dinh <dndinh7@gmail.com>
2023-04-09 18:49:09 -0400

    created the skeleton of the viewpost activity for a hardcoded post id and hard coded user id

--- bico-reuse-android/app/src/main/java/edu/brynmawr/cmsc353/bicoreuse/PostActivity.java
+++ bico-reuse-android/app/src/main/java/edu/brynmawr/cmsc353/bicoreuse/PostActivity.java
@@ -1,19 +1,79 @@
 package edu.brynmawr.cmsc353.bicoreuse;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import edu.brynmawr.cmsc353.bicoreuse.databinding.ActivityPostBinding;
+import edu.brynmawr.cmsc353.bicoreuse.info.PostInfo;
+import edu.brynmawr.cmsc353.bicoreuse.info.UserInfo;
 
+@SuppressWarnings("ALL")
 public class PostActivity extends AppCompatActivity {
+    private TextView tvTitle;
+    private TextView tvSeller;
+    private TextView tvDate;
+    private TextView tvDescription;
+    private TextView tvEmail;
+    private TextView tvPhone;
+    private Button btnDelete;
+    private Button btnEdit;
+
+    private UserInfo curUser;
+
+    private PostInfo postInfo= null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
-        ActivityPostBinding binding= DataBindingUtil.setContentView(this, R.layout.activity_post);
+
+
+        Intent intent= getIntent();
+        String postId= intent.getStringExtra("postId");
+        String curUserId= intent.getStringExtra("curUserId");
+        curUser= new UserInfo("6418b08bddfa69be902df5f3");
+        postInfo= new PostInfo("6418b08bddfa69be902df5f4");
+
+
+
+        tvTitle= findViewById(R.id.tvTitle);
+        tvSeller= findViewById(R.id.tvSeller);
+        tvDate= findViewById(R.id.tvDate);
+        tvDescription= findViewById(R.id.tvDescription);
+        tvEmail= findViewById(R.id.tvEmail);
+        tvPhone= findViewById(R.id.tvPhone);
+        btnDelete= findViewById(R.id.btnDelete);
+        btnEdit= findViewById(R.id.btnEdit);
+
+        tvTitle.setText(String.format("%s - $%.2f", postInfo.getTitle(), postInfo.getPrice()));
+        tvSeller.setText(String.format("Seller: %s", postInfo.getSeller().getName()));
+        tvEmail.setText(String.format("Email: %s", postInfo.getSeller().getEmail()));
+        tvPhone.setText(String.format("Phone: %s", postInfo.getSeller().getPhone()));
+        tvDate.setText(String.format("Posted: %s", postInfo.getDate()));
+        tvDescription.setText(postInfo.getDescription());
+
+        // set button visibilities if the user == seller
+        if (postInfo.getSeller().getId().equals(curUser.getId())) {
+            btnDelete.setVisibility(View.VISIBLE);
+            btnEdit.setVisibility(View.VISIBLE);
+        } else {
+            btnDelete.setVisibility(View.GONE);
+            btnEdit.setVisibility(View.GONE);
+        }
+
+    }
+
+    public void onDeleteButtonClick(View v) {
+
+    }
+
+    public void onEditButtonClick(View v) {
+
     }
 
 
--- bico-reuse-android/app/src/main/java/edu/brynmawr/cmsc353/bicoreuse/info/PostInfo.java
+++ bico-reuse-android/app/src/main/java/edu/brynmawr/cmsc353/bicoreuse/info/PostInfo.java
@@ -1,18 +1,123 @@
 package edu.brynmawr.cmsc353.bicoreuse.info;
 
+import android.util.Log;
+
+import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DateFormat;
 import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class PostInfo {
-    private String postId;
-    private String postTitle;
-    private String postDescription;
-    private Integer postPrice;
-    private Date postDate;
-    private String postStatus;
-    private UserInfo postSeller;
 
-    public PostInfo(String id, String title, String description, Integer price,
-                    Date date, String status, UserInfo seller) {
 
+    public final String id;
+    private String title;
+    private String description;
+    private Double price;
+    private String date;
+    private String status;
+    private UserInfo seller;
+
+    private boolean success;
+
+    public PostInfo(String id) {
+        this.id= id;
+        this.updateData();
+
+    }
+
+
+    public boolean updateData() {
+        success= false;
+
+        try {
+            ExecutorService executor = Executors.newSingleThreadExecutor();
+            executor.execute( () -> {
+                try {
+
+                    URL url = new URL("http://10.0.2.2:3000/post?pid=" + id);
+
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("GET");
+
+                    int responseCode = conn.getResponseCode();
+
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        InputStream in = conn.getInputStream();
+                        JSONParser jsonParser= new JSONParser();
+                        JSONObject jsonObject= (JSONObject)jsonParser.parse(
+                                new InputStreamReader(in, "UTF-8"));
+
+                        this.title= (String) jsonObject.get("title");
+                        this.description= (String) jsonObject.get("description");
+                        this.price= ((Number) jsonObject.get("price")).doubleValue();
+                        this.date= (String) jsonObject.get("date");
+                        this.status= (String) jsonObject.get("status");
+
+
+
+                        JSONObject sellerObject= (JSONObject) jsonObject.get("seller");
+                        this.seller= new UserInfo(sellerObject);
+
+                        success= true;
+
+                    } else {
+                        success= false;
+                        // Handle error response from server
+                    }
+                }
+                catch (Exception e) {
+                    Log.v("LoginInfo", e.getMessage());
+                    e.printStackTrace();
+                }
+            });
+
+            // this waits for up to 2 seconds
+            // it's a bit of a hack because it's not truly asynchronous
+            // but it should be okay for our purposes (and is a lot easier)
+            executor.awaitTermination(2, TimeUnit.SECONDS);
+
+        }
+        catch (Exception e) {
+            // uh oh
+            e.printStackTrace();
+        }
+        return success;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public String getDate() {
+        String d= String.format("%s/%s/%s", date.substring(5,7), date.substring(8,10), date.substring(0, 4));
+        return d;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public UserInfo getSeller() {
+        return seller;
     }
 }
